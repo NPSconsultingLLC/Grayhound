8512b5d5792d0b747eb74c759c2e8c0e2d620d29
Nathan Stryker <Nathan@optum.com>
2018-08-21 15:42:35 -0500

    removed storyboard, and added views programmatically

--- Greyhound/ViewController.swift
+++ Greyhound/ViewController.swift
@@ -10,18 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var playBtn: UIButton!
-    @IBOutlet weak var stopBtn: UIButton!
-    @IBOutlet weak var titleView: UIView!
-    @IBOutlet weak var contentView: UIView!
-    @IBOutlet weak var controlsView: UIView!
+    var titleLabel: UILabel!
+    var playBtn: UIButton!
+    var stopBtn: UIButton!
+    var titleView: UIView!
+    var contentView: UIView!
+    var controlsView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "This is the title"
-        playBtn.titleLabel?.text = "Play Button"
-        stopBtn.titleLabel?.text = "Stop Button"
+        self.view.backgroundColor = UIColor.darkGray
+        
+//
+
+//        titleLabel.text = "This is the title"
+//        playBtn.titleLabel?.text = "Play Button"
+//        stopBtn.titleLabel?.text = "Stop Button"
+        
+        
+        titleView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 75))
+        titleView.backgroundColor = UIColor.blue
+        self.view.addSubview(titleView)
+        
+        contentView = UIView(frame: CGRect(x: 0, y: 75, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        contentView.backgroundColor = UIColor.orange
+        self.view.addSubview(contentView)
+        
+        controlsView = UIView(frame: CGRect(x: 0, y: self.view.frame.size.height - 75, width: self.view.frame.size.width, height: 75))
+        controlsView.backgroundColor = UIColor.gray
+        self.view.addSubview(controlsView)
+        
+        self.setupPositions()
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +54,16 @@ class ViewController: UIViewController {
     
     @IBAction func onStopTapped(_ sender: Any) {
         //method for what happens when stop is tapped
+    }
+    
+    func setupPositions(){
+        
+//        titleView.translatesAutoresizingMaskIntoConstraints = true
+//        titleView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+//        titleView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+//        titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        //titleView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 3).isActive = true
+        
     }
 }
 
